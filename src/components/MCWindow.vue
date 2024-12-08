488c56e25cb38a49b3fa0986432c9bf6fec59518
Mojtaba Abbasi <Mabbasi@noornet.net>
2024-12-08 21:11:27 +0330

    درست جابه جایی گردآوری اطلاعات با قفسه داده

--- src/components/MCWindow.vue
+++ src/components/MCWindow.vue
@@ -8,44 +8,29 @@ const emit = defineEmits<Emit>()
 interface Emit {
   (e: 'close'): void // ایونت جدید close اضافه شد
   (e: 'open'): void // ایونت جدید close اضافه شد
+  (e: 'move'): void
 }
 </script>
 
 <template>
   <div class="content-box">
     <div class="content">
-      <VRow
-        no-gutters
-        justify="space-between"
-        align="center"
-        class="tool-bar"
-      >
+      <VRow no-gutters justify="space-between" align="center" class="tool-bar">
         <div class="title">
           {{ props.title }}
         </div>
         <div>
-          <VRow
-            no-gutters
-            class="btn-box"
-          >
-            <IconBtn
-              size="small"
-              @click="emit('open')"
-            >
-              <VIcon
-                icon="tabler-settings"
-                size="18"
-              />
+          <VRow no-gutters class="btn-box">
+            <IconBtn size="small" @click="emit('move')">
+              <VIcon icon="tabler-arrows-down-up" size="18" />
             </IconBtn>
-            <IconBtn
-              size="small"
-              @click="emit('close')"
-            >
-              <VIcon
-                icon="tabler-x"
-                size="18"
-              />
+            <IconBtn size="small" @click="emit('open')">
+              <VIcon icon="tabler-settings" size="18" />
             </IconBtn>
+            <IconBtn size="small" @click="emit('close')">
+              <VIcon icon="tabler-x" size="18" />
+            </IconBtn>
+
           </VRow>
         </div>
       </VRow>
--- src/pages/rs/main.vue
+++ src/pages/rs/main.vue
@@ -1,72 +1,42 @@
 <script setup lang="ts">
-import { SearchResultTabBoxModel } from '@/types/SearchResult';
 import { Pane, Splitpanes } from 'splitpanes';
 import 'splitpanes/dist/splitpanes.css';
 
 const menu = ref(false);
-const collectingdataitems = reactive<SearchResultTabBoxModel>({
-  id: 0,
-  content: [
-    { id: 0, title: 'حدیث', content: [{ id: 0, text: 'عَلِيُّ بْنُ مُحَمَّدٍ عَنْ سَهْلِ بْنِ زِيَادٍ عَنْ عَمْرِو بْنِ عُثْمَانَ عَنْ مُفَضَّلِ بْنِ صَالِحٍ عَنْ سَعْدِ بْنِ طَرِيفٍ عَنِ اَلْأَصْبَغِ بْنِ نُبَاتَةَ عَنْ عَلِيٍّ عَلَيْهِ اَلسَّلاَمُ قَالَ: هَبَطَ جَبْرَئِيلُ عَلَى آدَمَ عَلَيْهِ اَلسَّلاَمُ فَقَالَ يَا آدَمُ إِنِّي أُمِرْتُ أَنْ أُخَيِّرَكَ وَاحِدَةً مِنْ ثَلاَثٍ فَاخْتَرْهَا وَ دَعِ اِثْنَتَيْنِ فَقَالَ لَهُ آدَمُ يَا جَبْرَئِيلُ وَ مَا اَلثَّلاَثُ فَقَالَ اَلْعَقْلُ وَ اَلْحَيَاءُ وَ اَلدِّينُ فَقَالَ آدَمُ إِنِّي قَدِ اِخْتَرْتُ اَلْعَقْلَ فَقَالَ جَبْرَئِيلُ لِلْحَيَاءِ وَ اَلدِّينِ اِنْصَرِفَا وَ دَعَاهُ - فَقَالاَ يَا جَبْرَئِيلُ إِنَّا أُمِرْنَا أَنْ نَكُونَ مَعَ اَلْعَقْلِ حَيْثُ كَانَ قَالَ فَشَأْنَكُمَا وَ عَرَجَ .' }, { id: 0, text: 'این یک متن تستی شماره دو حدیث است' }, { id: 0, text: 'این یک متن تستی شماره سه  حدیث است' }] },
-    { id: 1, title: 'قران', content: [{ id: 0, text: 'این یک متن تستی شماره دو +-قران است' }, { id: 0, text: 'این یک متن تستی شماره یک قرآناست' }] },
-    { id: 2, title: 'ترجمه', content: [{ id: 0, text: 'این یک متن تستی  ترجمه است' }] },
-  ],
-})
-const infoSearch = ref();
-function getInfoSearch() { }
+const isComponentSwitch = ref(true);
+function changeWindowTitle(status: Boolean) {
+  return status ? 'گردآوری اطلاعات' : "قفسه داده";
+}
 </script>
 
 <template>
-    <div class="main">
-        <Splitpanes style="block-size: calc(100vh - 157px);" :rtl="true" class="default-theme">
-            <Pane size="30">
-                <MCWindow title="درخت خانواده" @open="menu = true" class="family-tree">
-                    <MCMainTree />
-                </MCWindow>
-            </Pane>
+  <div class="main">
+    <Splitpanes style="block-size: calc(100vh - 157px);" :rtl="true" class="default-theme">
+      <Pane size="30">
+        <MCWindow title="درخت خانواده" @close="menu = true" class="family-tree">
+          <MCMainTree />
+        </MCWindow>
+      </Pane>
 
       <Pane>
-        <Splitpanes
-          horizontal
-          rtl
-          class="default-theme"
-        >
+        <Splitpanes horizontal rtl class="default-theme">
           <Pane>
-            <MCWindow
-              title="گردآوری اطلاعات"
-              @open="menu = true"
-            >
+            <MCWindow :title="changeWindowTitle(isComponentSwitch)" @move="isComponentSwitch = !isComponentSwitch">
               <template #default>
-                <VContainer>
-                  <VRow>
-                    <VCol
-                      cols="12"
-                      md="6"
-                      class="mx-auto"
-                    >
-                      <VTextField
-                        v-model="infoSearch"
-                        placeholder="جستجو"
-                        append-inner-icon="mdi-magnify"
-                        class="search-bar"
-                        single-line
-                        @click:append-inner="getInfoSearch"
-                      />
-                    </VCol>
-                  </VRow>
-                </VContainer>
-                //TODO - تست کامپوننت تب بار، این کد موقتی است
-                <MCSearchResultTabBox :dataitems="collectingdataitems" />
-                <MCSearchResultTabBox :dataitems="collectingdataitems" />
+                <MCMainSearchInfo v-show="isComponentSwitch" />
+                <MCMainDataShelf v-show="isComponentSwitch == false" />
               </template>
             </MCWindow>
           </Pane>
 
           <Pane>
-            <MCWindow
-              title="قفسه داده"
-              @open="menu = true"
-            />
+            <MCWindow :title="changeWindowTitle(isComponentSwitch == false)"
+              @move="isComponentSwitch = !isComponentSwitch">
+              <template #default>
+                <MCMainSearchInfo v-show="isComponentSwitch == false" />
+                <MCMainDataShelf v-show="isComponentSwitch" />
+              </template>
+            </MCWindow>
           </Pane>
         </Splitpanes>
       </Pane>
