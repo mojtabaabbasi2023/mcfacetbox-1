3a33011e78f41d5e401d673dbc464a6b6c9ecaa1
Mojtaba Abbasi <Mabbasi@noornet.net>
2026-04-09 13:23:07 +0330

    add flat type virtual scroll

--- src/MCFacetFlat.vue
+++ src/MCFacetFlat.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { computed } from 'vue'
-import { VList, VListItem, VListItemAction, VCheckbox } from 'vuetify/components'
+import { VList, VListItem, VListItemAction, VCheckbox, VVirtualScroll, VListItemTitle } from 'vuetify/components'
 import type { IFacetItem } from './types'
 
 interface Props {
@@ -23,25 +23,37 @@ const internalValue = computed({
 </script>
 
 <template>
-  <VList
-    v-model:selected="internalValue"
-    item-value="key"
-    item-title="title"
-    lines="one"
-    select-strategy="leaf"
-    :return-object="false"
-    :height="(items.length ?? 10) * 36"
-    :max-height="(props.scrollItemCount ?? 10) * 35"
-  >
+  <!-- <VList v-model:selected="internalValue" item-value="key" item-title="title" lines="one" select-strategy="leaf"
+    :return-object="false" :height="(items.length ?? 10) * 36" :max-height="(props.scrollItemCount ?? 10) * 35">
     <VListItem v-for="item in items" :key="item.key" :title="item.title" :value="item.key">
       <template #prepend="{ isSelected }">
         <VListItemAction start>
           <VCheckbox :model-value="isSelected" density="compact" hide-details />
         </VListItemAction>
       </template>
-      <template #append>
+<template #append>
         <span class="px-2">{{ item.count }}</span>
       </template>
-    </VListItem>
-  </VList>
+</VListItem>
+</VList> -->
+
+  <v-list v-model:selected="internalValue" item-value="key" item-title="title" lines="one" select-strategy="leaf"
+    :return-object="false">
+    <v-virtual-scroll :items="items" item-height="28" :height="(props.scrollItemCount ?? 10) * 28">
+      <template #default="{ item }">
+        <v-list-item :value="item.key">
+          <template #prepend="{ isSelected, select }">
+            <v-list-item-action start>
+              <v-checkbox :model-value="isSelected" @update:model-value="select" density="compact" hide-details />
+            </v-list-item-action>
+          </template>
+
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+          <template #append>
+            <span class="px-2">{{ item.count }}</span>
+          </template>
+        </v-list-item>
+      </template>
+    </v-virtual-scroll>
+  </v-list>
 </template>
