9559ff0c437a2e9e9705ed8bf8a5bc44864e2333
Mojtaba Abbasi <Mabbasi@noornet.net>
2026-04-09 13:19:34 +0330

    reduce the size of the switch

--- src/defaults.ts
+++ src/defaults.ts
@@ -24,7 +24,7 @@ export default {
     density: 'comfortable',
     hideDetails: 'auto',
   },
- 
+
   VSelect: {
     variant: 'outlined',
     density: 'compact',
@@ -42,12 +42,13 @@ export default {
     color: 'primary',
     hideDetails: 'auto',
   },
-  
+
   VSwitch: {
     // set v-switch default color to primary
     inset: true,
     color: 'primary',
     hideDetails: 'auto',
+    density: 'compact',
   },
-  
+
 }
